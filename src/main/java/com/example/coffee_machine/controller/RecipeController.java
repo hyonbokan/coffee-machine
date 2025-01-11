5c7a8b6c374ba1a09a37884def24f09426edec08
hyonbokan <mkkanhb@gmail.com>
2025-01-11 20:49:32 +0900

    update readme and create recipe

--- src/main/java/com/example/coffee_machine/controller/RecipeController.java
+++ src/main/java/com/example/coffee_machine/controller/RecipeController.java
@@ -1,10 +1,15 @@
 package com.example.coffee_machine.controller;
+import com.example.coffee_machine.dto.ErrorResponseDto;
 import com.example.coffee_machine.dto.RecipeDto;
 import com.example.coffee_machine.service.RecipeService;
+import com.example.coffee_machine.utils.CoffeeMachineAvailabilityChecker;
 
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +22,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private final CoffeeMachineAvailabilityChecker coffeeMachineAvailabilityChecker;
     
     @PostMapping("/create")
-    public ResponseEntity<RecipeDto> createRecipe(@RequestBody RecipeDto recipeDto) {
+    public ResponseEntity<?> createRecipe(@RequestBody RecipeDto recipeDto) {
+
+        if (!coffeeMachineAvailabilityChecker.isOperational()) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(new ErrorResponseDto(
+                    "The coffee machine is not operational at this time.",
+                    HttpStatus.SERVICE_UNAVAILABLE.value(),
+                    LocalDateTime.now()
+                ));
+        }
+
         RecipeDto newRecipe = recipeService.createRecipe(recipeDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newRecipe);
     }
